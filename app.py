898e3c27ba7ed56ed2fdfa33f58d988af2d1868e
reysiregaradrian-commits <reysiregaradrian@gmail.com>
2026-04-30 13:29:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=app.config.get("DEBUG", False))
+    app.run(debug=True, port=Config.APP_PORT)
